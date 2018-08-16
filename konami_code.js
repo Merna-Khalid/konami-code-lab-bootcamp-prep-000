545ce542e025e5380899375b29d936314edbdbfc
Merna khaled <merna.khaled.5049@gmail.com>
2018-08-16 23:16:04 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -15,20 +15,34 @@ let index = 0
 function onKeyDownHandler(e) {
   const key = e.key
   alert(key)
-  if (key === codes[index]) {
-    index++;
-    if (index === codes.length) {
-      alert("Hurray!");
-
-      index = 0;
-    }
+  if(key === codes[0] && (index === 0 || index === 1)) {
+    index++
+  }
+  else if(key === codes[2] && (index === 2 || index === 3)) {
+    index++
+  }
+  else if(key === codes[4] && (index === 4 || index === 6)) {
+    index++
+  }
+  else if(key === codes[5] && (index === 5 || index === 7)) {
+    index++
+  }
+  else if(key === codes[8] && index === 8) {
+    index++
+  }
+  else if(key === codes[9] && index === 9) {
+    index++
   }
   else {
-    index = 0;
+    index = 0
   }
   //if(index != 0) {
   //  alert("Correct!")
   //}
+  if(index === codes.length) {
+    index = 0
+    alert("Hurray!")
+  }
 }
 
 function init() {
