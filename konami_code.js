02753bfa09dcc3886d3ab514df1ed381caca70d5
Merna khaled <merna.khaled.5049@gmail.com>
2018-08-16 23:06:01 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -10,7 +10,47 @@ const codes = [
   "b",
   "a"
 ];
+let index = 0
+
+function onKeyDownHandler(e) {
+  const key = e.key
+  alert(key)
+  if(key === codes[0] && (index === 0 || index === 1)) {
+    index++
+  }
+  else if(key === codes[2] && (index === 2 || index === 3)) {
+    index++
+  }
+  else if(key === codes[4] && (index === 4 || index === 6)) {
+    index++
+  }
+  else if(key === codes[5] && (index === 5 || index === 7)) {
+    index++
+  }
+  else if(key === codes[8] && index === 8) {
+    index++
+  }
+  else if(key === codes[9] && index === 9) {
+    index++
+  }
+  else {
+    index = 0
+  }
+
+  if(index === codes.length) {
+    alert("Hurray!")
+    index = 0
+  }
+  else if(index != 0) {
+    alert("Correct!")
+  }
+}
 
 function init() {
   // your code here
+  document.onkeydown = onKeyDownHandler
+  //var main = document.getElementsByTagName("html")
+  //main.addEventListener("keydown", onKeyDownHandler, false)
+  //alert("yalaaahwiiii")
+
 }
